f74e16a535c2cfbbbcaaedb1628574a8257c3018
elmira <esenalievaelmira011@gmail.com>
2024-11-20 20:44:01 +0600

    Update

--- app.js
+++ app.js
@@ -1,35 +1,56 @@
-// const cardImg =[
-//     'https://trikky.ru/wp-content/blogs.dir/1/files/2022/08/31/1621660832-9-phonoteka-org-p-sve.jpg',
-//     'https://twam.ru/wp-content/uploads/2024/03/nebo-estetika-3.webp',
-//     'https://sotni.ru/wp-content/uploads/2023/08/rozovoe-oblako-161.webp',
-//     'https://sotni.ru/wp-content/uploads/2023/08/rozovoe-oblako-145.webp'
-// ]
 
-function cards (product){
-    const contAiner = document.getElementsByClassName('.container');
-    const card1 = document.createElement('div');
-    card1.className = 'product-card';
+        // создаем карточку
+        function createCard(imageUrl, title, description) {
+            const card = document.createElement('div');
+            card.className = 'card';
 
-    const image = document.createElement('img');
-    image.src = product.image;
-    image.className = 'https://sotni.ru/wp-content/uploads/2023/08/rozovoe-oblako-145.webp';
-    card1.appendChild(image)
+            // Добавляем изображение
+            const img = document.createElement('img');
+            img.src = imageUrl;
+            img.alt = title;
+            card.appendChild(img);
 
-    const name = document.createElement('h4');
-    name.textContent = product.name;
-    name.className = 'product-name';
-    card1.appendChild(name);
+            // инфо внутри карты
+            const content = document.createElement('div');
+            content.className = 'card-content';
 
-    const price = document.createElement('p');
-    price.textContent = `${product.price} p`;
-    price.className ="product-price";
-    card1.appendChild(price);  
-}
+            const cardTitle = document.createElement('h3');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = title;
 
-const product = {
-    name: "LapTope",
-    price: "50000",
-    image: "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RW16e0d?ver=055e/100x200"
-}
+            const cardDescription = document.createElement('a');
+            cardDescription.className = 'card-description';
+            cardDescription.textContent = description;
 
-cards(product);
+            // const button = document.createElement('a');
+            // button.className = 'card-button';
+            // button.textContent = buttonText;
+            // button.href = '#';
+
+            //  содержимое карточкb
+            content.appendChild(cardTitle);
+            content.appendChild(cardDescription);
+            card.appendChild(content);
+
+            // Добавляем карточку на страницу
+            document.getElementById('card-container').appendChild(card);
+        }
+
+        createCard(
+            'https://cdn2.unrealengine.com/remix-key-art-1920x1080-0b23bdca2f5f.jpg',
+            'Free-To-Play',
+            'Подробнее...',
+        );
+
+        createCard(
+            'https://i.ytimg.com/vi/v-vqi0UaUdE/maxresdefault.jpg',
+            'Car Racing',
+            'Подробнее...',
+        );
+
+        createCard(
+            'https://www.cdmi.in/courses@2x/2D3D-Game-Design.webp',
+            'Game Design Course',
+            'Подробнее...',
+        );
+
